ae9254e37475e32bd38f7e95f0c196fc2f76ec49
aishwaryadevi-04 <aishwaryadev2004@gmail.com>
2023-07-14 23:25:01 +0530

    first commit

--- src/components/home/fetchimage.js
+++ src/components/home/fetchimage.js
@@ -1,33 +1,31 @@
-
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const FetchImage = ({ blogs, setBlogs }) => {
   const accessKey = 'XGNAAV1ImuCpEkZsITer1maM-nKnHJPhoQO_O6f9-FQ';
-  const searchQuery = 'technology';
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://api.unsplash.com/search/photos?query=${searchQuery}&client_id=${accessKey}`);
-        const fetchedImages = response.data.results;
+        const updatedBlogs = await Promise.all(
+          blogs.map(async (blog) => {
+            const response = await axios.get(
+              `https://api.unsplash.com/search/photos?query=${blog.category}&client_id=${accessKey}`
+            );
+            const fetchedImages = response.data.results;
 
-        
-        const updatedBlogs = blogs.map((blog, index) => {
-          if (index < fetchedImages.length) {
-           
-            return {
-              ...blog,
-              imageUrl: fetchedImages[index].urls.regular
-            };
-          } else {
-           
-            return blog;
-          }
-        });
+            if (fetchedImages.length > 0) {
+              return {
+                ...blog,
+                imageUrl: fetchedImages[0].urls.regular,
+              };
+            } else {
+              return blog;
+            }
+          })
+        );
 
         setBlogs(updatedBlogs);
-
       } catch (error) {
         console.error('Error fetching images from Unsplash:', error);
       }
